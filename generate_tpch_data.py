5d9be64e85dc525d02bf407a069153755777744f
Chris Zhu <chz@uchicago.edu>
2024-10-23 22:51:29 +0000

    finish gen tpch data

--- generate_tpch_data.py
+++ generate_tpch_data.py
@@ -1,10 +1,33 @@
 import os
 import subprocess
+import csv
 
 TPCH_GEN_DIR = "./TPC-H V3.0.1/dbgen"
 # DATASIZES = ['1MB', '10MB', '100MB', '1GB', '10GB']
 DATASIZES = ['1MB', '10MB']
 SPLIT_RATIOS = [0.09,0.5]
+
+def select_custkey(file_name, out_file):
+    """
+    Selects the custkey column from the input file and writes it to the output file
+    """
+    custkey_column = []
+    with open(file_name, 'r') as input_file:
+        # Read the content of the input file
+        content = csv.reader(input_file, delimiter='|')
+        # print(content)
+        for row in content:
+            custkey_column.append(row[1])
+            # print(row[1])
+
+        # Extract the custkey column from the content
+        # custkey_column = [line.split('|')[1] for line in content]
+        # print(custkey_column)
+    with open(out_file, 'w') as first_output_file:
+        writer = csv.writer(first_output_file, delimiter=',')
+        for custkey in custkey_column:
+            writer.writerow([custkey])
+
 
 # split 1MB ... 10GB orders.csv into two files of size ratio "ratio"
 def split_file(file_name, out_file, ratio):
@@ -30,12 +53,12 @@ def split_file(file_name, out_file, ratio):
         second_file_content = content[first_file_size:]
 
     # Create the first output file
-    first_file_path = out_path + "/" + out_file + '1.csv'
+    first_file_path = out_path + "/" + out_file + '1.tbl'
     with open(first_file_path, 'w') as first_output_file:
         first_output_file.writelines(first_file_content)
 
     # Create the second output file
-    second_file_path = out_path + "/" + out_file + '2.csv'
+    second_file_path = out_path + "/" + out_file + '2.tbl'
     with open(second_file_path, 'w') as second_output_file:
         second_output_file.writelines(second_file_content)
 
@@ -46,9 +69,9 @@ def generate_data(datasize, directory, datasize_dict):
     # cd to dbgen directory and generate data
     os.chdir(TPCH_GEN_DIR)
     tpch_dir = os.getcwd()
-    print(f"Generating data within {tpch_dir}")
+    print(f"Generating data within {tpch_dir} of size {str(datasize_dict[datasize])}")
 
-    subprocess.run(["./dbgen",str(datasize_dict[datasize])])
+    subprocess.run(["./dbgen","-s",str(datasize_dict[datasize])])
     print("Moving data to",directory)
     subprocess.run(f"mv *.tbl {directory}", shell=True)
 
@@ -79,7 +102,8 @@ def main():
     # split orders table for each datasize
     cwd = os.getcwd()
     for data_size, datasize_dir in zip(DATASIZES, datasize_dirs):
-        if all([os.path.exists(os.path.join(datasize_dir,f"split{ratio}/orders1.csv")) for ratio in SPLIT_RATIOS]):
+        print(f"Checking directory {datasize_dir}")
+        if all([os.path.exists(os.path.join(datasize_dir,f"split{ratio}/orders1.tbl")) for ratio in SPLIT_RATIOS]):
             print(f"Data already split for orders on {datasize}. Skipping data splitting.")
             continue
         os.chdir(datasize_dir)
@@ -87,6 +111,17 @@ def main():
             split_file("orders.tbl", "orders", ratio)
     # cd back to working directory
     os.chdir(cwd)
+    
+    # select custkey from the split orders table
+    for data_size, datasize_dir in zip(DATASIZES, datasize_dirs):
+        print(f"Checking directory {datasize_dir}")
+        if all([os.path.exists(os.path.join(datasize_dir,f"split{ratio}/orders1.csv")) for ratio in SPLIT_RATIOS]):
+            print(f"Data already split for custkey on {datasize}. Skipping data splitting.")
+            # continue
+        os.chdir(datasize_dir)
+        for ratio in SPLIT_RATIOS:
+            select_custkey(f"split{ratio}/orders1.tbl", f"split{ratio}/orders1.csv")
+            select_custkey(f"split{ratio}/orders2.tbl", f"split{ratio}/orders2.csv")
 
 
 if __name__ == "__main__":
