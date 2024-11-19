d3c43779053d06a5eb4f6cbfcff8a5d22709cf08
Chris Zhu <chz@uchicago.edu>
2024-11-19 05:59:39 +0000

    rerun join

--- plaintext_tpch_experiments/run_join_experiments.sh
+++ plaintext_tpch_experiments/run_join_experiments.sh
@@ -1,5 +1,6 @@
 for size in 1MB 10MB 100MB 1GB 10GB
 # for size in 1MB 10MB
+# for size in 10GB
 do
     echo "Running join_scenario_experiment for $size"
     python -m join $size 10 &> join_scenario_experiment_$size.log
