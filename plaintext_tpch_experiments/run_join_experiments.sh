2900ea221364b29e2fa7176665fc4d144370a2c7
Chris Zhu <chz@uchicago.edu>
2024-11-19 17:28:59 +0000

    rerun exps because 11 instead of 10 trials

--- plaintext_tpch_experiments/run_join_experiments.sh
+++ plaintext_tpch_experiments/run_join_experiments.sh
@@ -3,5 +3,5 @@ for size in 1MB 10MB 100MB 1GB 10GB
 # for size in 10GB
 do
     echo "Running join_scenario_experiment for $size"
-    python -m join $size 10 &> join_scenario_experiment_$size.log
+    python -m join $size 11 &> join_scenario_experiment_$size.log
 done
