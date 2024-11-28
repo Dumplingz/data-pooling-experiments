0162731949f931da863a2b9c01ca8422e829a028
Chris Zhu <chz@uchicago.edu>
2024-11-28 09:01:03 +0000

    plaintext join

--- plaintext_tpch_experiments/join.py
+++ plaintext_tpch_experiments/join.py
@@ -16,6 +16,37 @@ if __name__ == '__main__':
     for agent in ["1", "2"]:
         agent_des.append(f"../tpch_workdir/{num_MB}/split0.5/orders{agent}.tbl")
 
+    table_query_load = f"""
+            CREATE TABLE ORDERS1 AS 
+            SELECT * FROM read_csv('{agent_des[0]}',
+                delim = '|', header = false,
+                columns = {{
+                    'O_ORDERKEY': 'INTEGER NOT NULL',
+                    'O_CUSTKEY': 'INTEGER NOT NULL',
+                    'O_ORDERSTATUS': 'CHAR(1) NOT NULL',
+                    'O_TOTALPRICE': 'DECIMAL(15,2) NOT NULL',
+                    'O_ORDERDATE': 'DATE NOT NULL',
+                    'O_ORDERPRIORITY': 'CHAR(15) NOT NULL',
+                    'O_CLERK': 'CHAR(15) NOT NULL',
+                    'O_SHIPPRIORITY': 'INTEGER NOT NULL',
+                    'O_COMMENT': 'VARCHAR(79) NOT NULL'
+                    }});
+            CREATE TABLE ORDERS2 AS
+            SELECT * FROM read_csv('{agent_des[1]}',
+                delim = '|', header = false,
+                columns = {{
+                    'O_ORDERKEY': 'INTEGER NOT NULL',
+                    'O_CUSTKEY': 'INTEGER NOT NULL',
+                    'O_ORDERSTATUS': 'CHAR(1) NOT NULL',
+                    'O_TOTALPRICE': 'DECIMAL(15,2) NOT NULL',
+                    'O_ORDERDATE': 'DATE NOT NULL',
+                    'O_ORDERPRIORITY': 'CHAR(15) NOT NULL',
+                    'O_CLERK': 'CHAR(15) NOT NULL',
+                    'O_SHIPPRIORITY': 'INTEGER NOT NULL',
+                    'O_COMMENT': 'VARCHAR(79) NOT NULL'
+                    }});
+    """
+    
     table_query = """
     CREATE TABLE ORDERS1  ( O_ORDERKEY       INTEGER NOT NULL,
                 O_CUSTKEY        INTEGER NOT NULL,
@@ -74,7 +105,7 @@ COPY ORDERS2 FROM '{de2_filepath}' DELIMITER '|';
 
 SELECT DISTINCT o1.o_custkey FROM ORDERS1 o1 JOIN ORDERS2 o2 ON o1.o_custkey = o2.o_custkey;"""
     
-    query = full_query.format(de1_filepath=agent_des[0], de2_filepath=agent_des[1])
+    # query = full_query.format(de1_filepath=agent_des[0], de2_filepath=agent_des[1])
     # run setup queries
     # conn.execute(table_query)
     # conn.execute(load_query)
@@ -86,9 +117,11 @@ SELECT DISTINCT o1.o_custkey FROM ORDERS1 o1 JOIN ORDERS2 o2 ON o1.o_custkey = o
     for _ in range(num_trials):
         start_time = time.perf_counter()
         conn = duckdb.connect()
+        
+        conn.execute(table_query_load)
 
         # res = conn.execute(join_query).fetchall()
-        res = conn.execute(query).fetchdf()
+        res = conn.execute(join_query).fetchdf()
         end_time = time.perf_counter()
         total_time = end_time - start_time
         # i = 0
